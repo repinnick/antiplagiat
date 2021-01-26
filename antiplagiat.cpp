b66c2cf7a18044a203c5e4650753ecb808a6acad
igotbitches <check.point9917@yandex.ru>
2021-01-26 19:58:31 +0300

    feat: create new function for redact text

--- antiplagiat.cpp
+++ antiplagiat.cpp
@@ -2,24 +2,20 @@
 #include <string>
 #include <cctype>
 
-
 using namespace std;
 
 bool isSeparator(char c);
+string textWithoutSeparators(string text);
 
 int main()
 {
-	string text = "Children start school at the age of five, but there is some free nursery-school education before that age. The state nursery schools are not for all. They are for some families, for example for families with only one parent. In most areas there are private nursery schools. Parents who want their children to go to nursery school pay for their children under 5 years old to go to these private nursery schools.";
+    string text = "Children start school at the age of five, but there is some free nursery-school education before that age. The state nursery schools are not for all. They are for some families, for example for families with only one parent. In most areas there are private nursery schools. Parents who want their children to go to nursery school pay for their children under 5 years old to go to these private nursery schools.";
+    string redactText = textWithoutSeparators(text);
+    string threeWordsForCheck;
 
-	string textWithoutSeparators;
+    cout << redactText;
 
-	for (int i = 0; i < text.size(); i++){
-	    if(!isSeparator(text[i])){
-	        textWithoutSeparators += tolower(text[i]);
-	    }
-	}
-    cout << textWithoutSeparators;
-	return 0;
+    return 0;
 }
 
 bool isSeparator(char c)
@@ -31,4 +27,15 @@ bool isSeparator(char c)
             return true;
 
     return false;
+}
+
+string textWithoutSeparators(string text)
+{
+    string newText;
+    for (int i = 0; i < text.size(); i++) {
+        if (!isSeparator(text[i])) {
+            newText += tolower(text[i]);
+        }
+    }
+    return newText;
 }
